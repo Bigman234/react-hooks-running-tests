be1ca44f7cff8cd46a599d10650577cbcea0efd5
BIGMAN234 <daniel.bundotich@student.moringaschool.com>
2024-07-30 12:35:30 +0300

    initial commit

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,10 @@
 import React from "react";
 
 function Article() {
-  return (
-    <div>
-      {/* please pass this test */}
-    </div>
-  );
+  return <div>please pass this test</div>;
 }
+screen.debug();
+
+expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 
 export default Article;
